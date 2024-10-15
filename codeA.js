e912dd938f16a20e775724f8d3164ec818ef0030
Lanzm253 <lanzm253@gmail.com>
2024-10-15 18:57:53 +0800

    Delete codeA.js

--- codeA.js
+++
@@ -1,7 +0,0 @@
-window.CodeA = {
-    1: 'https://cdn.imgchest.com/files/j7kzczjjwr7.jpg',
-    2: 'https://mez.ink/clark',
-    3: 'https://mez.ink/HeytaekabaHAHAHAHAH',
-    4: 'https://boombastiksideeye.com',
-    5: 'https://mez.ink/Minecraft'
-};
